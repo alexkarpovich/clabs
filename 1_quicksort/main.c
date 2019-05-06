a38ec1b2e7939ca896f38ca325e3d676ae08f8be
Aliaksandr Karpovich <alexsure.k@gmail.com>
2019-05-06 12:03:09 +0300

    working quicksort

--- 1_quicksort/main.c
+++ 1_quicksort/main.c
@@ -1,25 +1,31 @@
+#include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "quicksort.c"
 
-const int ARRAY_LENGTH = 10;
+const long long ARRAY_LENGTH = 99999999;
 
-int main() {
-    srand(time(NULL));   // Initialization, should only be called once. 
-    int* arr = malloc(ARRAY_LENGTH * sizeof(int));
-    printf("Original:\n"); 
-    for (int i = 0; i < ARRAY_LENGTH; i++) {
-        arr[i] = rand() % 20;
-        printf("%d ", arr[i]); 
+int main() 
+{
+    int ret = 0;
+    long long* arr;
+    srand(time(NULL));
+    
+    if (NULL == (arr = malloc(ARRAY_LENGTH * sizeof(long long)))) {
+        ret = errno;
+        goto exit;
+    }
+
+    for (long long i = 0; i < ARRAY_LENGTH; i++) {
+        long long x = random();
+        arr[i] = x;
     }
 
     quicksort(arr, ARRAY_LENGTH);
-
-    printf("\nSorted:\n"); 
-    for (int i = 0; i < 10 ; i++){ 
-         printf("%d ", arr[i]); 
-    } 
-  
-    return 0; 
+    
+    exit:
+        free(arr);
+        return ret; 
 }
--- 1_quicksort/quicksort.c
+++ 1_quicksort/quicksort.c
@@ -1,39 +1,35 @@
 
-int partition(int* arr, int lid, int rid) {
-    int pivot = arr[(rid - lid) >> 1];    
-    int lidc = lid;
-    int ridc = rid;
+int partition(long long* arr, long long len) {
+    long long pivot = arr[0];    
+    long long lidc = 1;
+    long long ridc = len - 1;
 
     while (1) {
-        while (lidc < rid && arr[lidc] < pivot) ++lidc;
-        while (ridc > lid && arr[ridc] >= pivot) --ridc;
+        while (lidc <= ridc && arr[lidc] <= pivot) ++lidc;
+        while (ridc >= lidc && arr[ridc] >= pivot) --ridc;
 
-        if (lidc >= ridc) {
-            return lidc;
+        if (lidc > ridc) {
+            break;
         }
 
-        int tmp = arr[lidc];
+        long long tmp = arr[lidc];
         arr[lidc] = arr[ridc];
         arr[ridc] = tmp;        
     }
+
+    arr[0] = arr[ridc];
+    arr[ridc] = pivot; 
+
+    return ridc;
 }
 
-void quicksort_helper(int* arr, int lid, int rid) {
-    if (lid >= rid) {
+void quicksort(long long* arr, long long len) {
+    if (len < 2) {
         return;
     }
     
-    int sid = partition(arr, lid, rid);
+    long long sid = partition(arr, len);
 
-    if (lid < sid - 1) {
-        quicksort_helper(arr, lid, sid - 1);
-    }
-
-    if (rid > sid + 1) {
-        quicksort_helper(arr, sid + 1, rid);
-    }
-}
-
-void quicksort(int* arr, int len) {
-    quicksort_helper(arr, 0, len - 1);
+    quicksort(arr, sid);
+    quicksort(arr+sid+1, len-sid-1);
 }
