c2e05aa941381ace5ff1fd24c9f5e6acb8a9ee00
Aliaksandr Karpovich <alexsure.k@gmail.com>
2019-08-08 08:11:54 +0300

    Complete not-divisible-subset algorithm

--- 2_non_divisible_subset/main.c
+++ 2_non_divisible_subset/main.c
@@ -4,121 +4,27 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define CAP_SIZE 10
 #define min(a, b) (((a) < (b)) ? (a) : (b)) 
-
-
-typedef struct {
-    int len;
-    int cap;
-    int* d;
-} array;
-
-int max(int x, int y) { 
-    return x ^ ((x ^ y) & -(x < y));  
-} 
-
-array* init(int cap) {
-    array* arr = malloc(sizeof(array));
-    arr->len = 0;
-    arr->cap = cap;
-    arr->d = (int*)malloc(arr->cap * sizeof(int));
-
-    return arr;
-}
-
-void append(array* arr, int v) {
-    if (arr->len == arr->cap) {
-        arr->cap = arr->cap + CAP_SIZE;
-        arr->d = (int*)realloc(arr->d, arr->cap * sizeof(int));
-    }
-
-    arr->d[arr->len++] = v;
-}
-
-void del(array* arr, int v) {
-    if (arr->d[arr->len - 1] == v) --arr->len;
-    else {
-        for (int i = 0; i < arr->len; i++) {
-            if (arr->d[i] == v) {
-                memcpy(arr->d + i, arr->d + i + 1, (arr->len - i) * sizeof(int));
-                --arr->len;
-            }
-        }
-    }
-}
-
-int check_not(int k, array* candidates, array* not) {
-    int counter;
-
-    for (int i = 0; i < not->len; i++) {
-        counter = 0;
-
-        for (int j = 0; j < candidates->len; j++) {
-            if ((not->d[i] + candidates->d[j]) % k != 0) ++counter;
-        }
-
-        if (counter == candidates->len) return 0;
-    }
-
-    return 1;
-}
-
-void new_sets(int k, int v, array* candidates, array* not, array* new_candidates, array* new_not) {
-    int max_len = max(candidates->len, not->len);
-
-    for (int i = 0; i < max_len; i++) {
-        if (i < candidates->len && v != candidates->d[i] && (v + candidates->d[i]) % k != 0) {
-            append(new_candidates, candidates->d[i]);   
-        }
-
-        if (i < not->len && v != not->d[i] && (v + not->d[i]) % k != 0) {
-            append(new_not, not->d[i]);   
-        }
-    }
-}
-
-void extend(int* max_subset, int k, array* compsub, array* candidates, array* not) {
-    int v;
-
-    while (candidates->len > 0 && check_not(k, candidates, not)) {
-        v = candidates->d[0];
-        append(compsub, v);
-
-        array* new_candidates = init(CAP_SIZE), *new_not = init(CAP_SIZE);
-        new_sets(k, v, candidates, not, new_candidates, new_not);
-
-        if (!new_candidates->len && !new_not->len) {
-            if (compsub->len > *max_subset) *max_subset = compsub->len;
-        } else extend(max_subset, k, compsub, new_candidates, new_not);
-
-        free(new_candidates->d);
-        free(new_not->d);
-
-        del(candidates, v);
-        del(compsub, v);
-        append(not, v);
-    }
-}
-
+#define min(a, b) (((a) > (b)) ? (a) : (b)) 
 
 int process(int k, int s_count, int* s) {
-  int reminder, id, max_len = 0;
+  int count, max_len = 0;
   int* rmd = (int*)calloc(k, sizeof(int));
 
   for (int i = 0; i < s_count; i++) {
-    reminder = s[i] % k;
-    id = min(reminder, k - reminder);
-    ++rmd[reminder];
-
-    if (max_len < rmd[id]) max_len = rmd[id];
+    ++rmd[s[i] % k];
   }
 
-  for (int i = 0; i < k; i++) {
-      printf("[%d]%d ", i, rmd[i]);
+  count = min(rmd[0], 1);
+  for (int i = 1; i < k / 2 + 1; i++) {
+      if (i != k - i) {
+          count += max(rmd[i], rmd[k - i]);
+      }
   }
 
-  return max_len;
+  if (k % 2 == 0) ++count;
+
+  return count;
 }
 
 int main(void) {
@@ -161,13 +67,6 @@ int main(void) {
                 prev_i = i + 1;
         }
     }
-
-    // array* candidates = init(data[0] + CAP_SIZE), *not = init(CAP_SIZE), *compsub = init(CAP_SIZE);
-
-    // memcpy(candidates->d, data + 3, data[0] * sizeof(int));
-    // candidates->len = data[0];
-
-    // extend(&max_subset, data[1], compsub, candidates, not);
 
     max_subset = process(data[1], data[0], data + 3);
 
